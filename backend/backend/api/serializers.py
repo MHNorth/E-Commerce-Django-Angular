31cd683690a41596c4eb5f654f6bb380f6d2d671
Maika Northrop <maikanorthrop@gmail.com>
2019-02-03 20:06:57 -0500

    dev backend authorization

--- backend/backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-class UserSerializer(serializers.HyperlinkedModelSerializer): # Hyperlinked serializer allows the use of primary keys and various other relationships
-        class Meta:
-                model = User
-                fields = ('url', 'username', 'email')
